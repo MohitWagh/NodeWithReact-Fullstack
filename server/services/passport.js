639968459be79dcbc257e7a2c78671aa4e7044a3
mohit laptop <mohitvwagh@gmail.com>
2019-12-26 18:25:10 +0400

    Can add and extract unique users from mongo db

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,9 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
 
 //https://console.developers.google.com
 let googleStrategy = new GoogleStrategy({
@@ -11,7 +21,15 @@ let googleStrategy = new GoogleStrategy({
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        new User({googleID: profile.id}).save();
+        User.findOne({googleID: profile.id}).then((existingUser) => {
+            if (existingUser) {
+                //already have record of user
+                done(null, existingUser);
+            } else {
+                //need to create new user
+                new User({googleID: profile.id}).save().then(user => done(null, user));
+            }
+        });
     }
 );
 

b2e1a8a90dabab85fe79f86d7b7daaca68ba052e
mohit laptop <mohitvwagh@gmail.com>
2020-01-02 09:51:48 +0400

    Basic routing using header and header functionality added

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Landing extends React.Component{
+    render() {
+        return(
+            <div style={{textAlign:'center'}}>
+                <h1>
+                    Emaily!
+                </h1>
+                Collect feedback from your users.
+            </div>
+        );
+    }
+}
+
+export default Landing;
